ff907cbaf8cd5ac94253e4cf2eb5b5596b74f358
Mehmet Efe Özdemir <mefe0123sm@gmail.com>
2025-12-10 22:44:15 +0300

    Added Hypotenuse Calculator Script

---
+++ hypotenuse.py
@@ -0,0 +1,5 @@
+import math
+print(" dik bir üçgenin hipotenüsünü bulma programına hoş geldiniz")
+x = float(input("Üçgenin dik kenarlarından birini giriniz giriniz: "))
+y = float(input("Diğer kenarını giriniz: "))
+print("hipotenüs uzunluğu",math.sqrt(x*x + y*y))
