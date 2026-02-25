5735a17b976d3fb0ca6edd60f08f97aac7d3fec9
Mehmet Efe Özdemir <mefe0123sm@gmail.com>
2026-02-25 16:13:58 +0300

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,7 +1,7 @@
 print("Vücut Kitle İndeksi Hesaplama Makinesine Hoş Geldiniz Hesap Makinemiz SI Birimlerine Göre Çalışmaktadır.")
-kilo=float(input("Kilonuzu giriniz: ")) # kg ve m olması hem SI birimi için doğru hem bizi zahmete sokmaz
-boy=float(input("Boyunuzu giriniz (1.69 M, 1.81 M gibi): ")) # float burada önemli
-# boylarını 171 gibi yazdıklarında sorun çıkıyor nokta koymaları gerekir 1.71 gibisinden
+kilo=float(input("Kilonuzu giriniz: ")) 
+boy=float(input("Boyunuzu giriniz (1.69 M, 1.81 M gibi): "))
+
 # BMI means Body Mass Index
 if boy>3:
     print(f"⚠️ Uyarı: Boyunuzu cm olarak({boy})girdiniz, sistem otomatik olarak metreye çevirdi.")
@@ -9,7 +9,7 @@ if boy>3:
 bmi= kilo/(boy**2)
 
 print(f"---------------------------------\nVücut Kitle İndeksiniz: {bmi:.2f}")
-# :.2f demek, virgülden sonra sadece 2 basamak göster demek.
+
 if bmi<18.5:
     print("Zayıf. Sağlık Riski Bulunmakta")
 elif bmi<25 :
@@ -20,4 +20,5 @@ elif bmi<35 :
     print("Obez (Sınıf I). Dikkat!")
 else:
     print("Yüksek Obez Seviyesi. Doktora Danışın!")
+
 
